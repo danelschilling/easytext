b217b6a0c40f089e243d403da2c21337ad2dc210
Daniel <danielschilling.ssnfq@academico.ifsul.edu.br>
2022-03-18 17:51:07 -0300

    atualizando controllers

--- src/main/java/br/com/ifsul/easytext/controller/LicaoCotroller.java
+++ src/main/java/br/com/ifsul/easytext/controller/LicaoCotroller.java
@@ -1,11 +1,23 @@
 package br.com.ifsul.easytext.controller;
 
+import br.com.ifsul.easytext.controller.dto.UsuarioDto;
+import br.com.ifsul.easytext.models.Licao;
+import br.com.ifsul.easytext.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RequestMapping("/licao")
 @RestController
 public class LicaoCotroller {
 
+    @RequestMapping("/topicos")
+    public List<UsuarioDto> lista() {
+        Usuario usuario = new Usuario("Dúvida", "Dúvida com Spring", new Licao("Spring", "Programaação"));
+
+        return UsuarioDto.converter(Arrays.asList(usuario, usuario, usuario));
+    }
 
 }
